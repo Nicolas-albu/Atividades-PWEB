1267cba2ae83ad42de4e0c037f2ea39a894701aa
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-03-10 10:17:47 -0300

    Update

--- Atividade 01/script.js
+++ Atividade 01/script.js
@@ -1,3 +1,5 @@
-buttonSeparated(){
-
+function buttonSeparated(){
+    nomeCompleto = document.getElementById("campo-de-nome-completo").value.split(" ");
+    document.getElementById("campo-de-nome").value = nomeCompleto[0];
+    document.getElementById("campo-de-sobrenome").value = nomeCompleto[1];
 };
