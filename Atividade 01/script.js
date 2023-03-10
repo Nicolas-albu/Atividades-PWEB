af61e45ab3f0d24f6fd0a39b972f1c8e726e6416
Nícolas Albuquerque R <nicolasalbuquerque581@gmail.com>
2023-03-10 15:45:38 -0300

    update

--- Atividade 01/script.js
+++ Atividade 01/script.js
@@ -1,16 +1,24 @@
-//https://www.alura.com.br/artigos/trocando-caracteres-de-uma-string-no-java#:~:text=M%C3%A9todo%20replaceAll(),que%20passarmos%20por%20outro%20caractere.?utm_source=awin&utm_medium=site&utm_term=101248&awc=23465_1678467659_5f104ce2c64fa8a3922f21a302913942
-//https://github.com/Lea1dev/displayName-JavaScript/blob/main/script.js
+function enterBotao(event){
+    //verifica se a tecla ENTER foi pressionada
+    if(event.keyCode === 13) {
+        tabButton();
+    }
+}
 
-function tratamentoNomeCompleto(nomeCompleto) {
-    return nomeCompleto.replace(/ \s+/g, " ").trim();
+function verificarNomeCompleto(nomeCompleto) {    
+    const regex = /\d+/; //verifica se tem números
+    return !regex.test(nomeCompleto);
 };
 
-function buttonSeparated(){
-    let nomeCompleto = document.getElementById("campo-de-nome-completo").value;
-    let nomeCompletoTratado = this.tratamentoNomeCompleto(nomeCompleto);
-    let nomeCompletoSeparado = nomeCompletoTratado.split(" ");
-    document.getElementById("campo-de-nome").value = nomeCompletoSeparado[0];
-    let sobrenome = nomeCompletoSeparado.slice(1);
+function tabButton(){
+    let nomeCompleto = document.getElementById("campo-de-nome-completo").value.trim();    
+    if (verificarNomeCompleto(nomeCompleto)){
+        let nomeCompletoSeparado = nomeCompleto.split(" ");
+        let sobrenome = nomeCompletoSeparado.slice(1);
 
-    document.getElementById("campo-de-sobrenome").value = sobrenome.join(" ");
+        document.getElementById("campo-de-nome").value = nomeCompletoSeparado[0];
+        document.getElementById("campo-de-sobrenome").value = sobrenome.join(" ");
+    } else {
+        alert("Não pode inserir número no campo de nome completo");
+    }
 };
