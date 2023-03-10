40b8b4f01c4da92b1671e65baacf32dcdaacf160
nicolasalbuquerque581@gmail.com <2021325683@IFAL-MACEIO.INTRANET>
2023-03-10 14:27:21 -0300

    update

--- Atividade 01/script.js
+++ Atividade 01/script.js
@@ -1,5 +1,16 @@
+//https://www.alura.com.br/artigos/trocando-caracteres-de-uma-string-no-java#:~:text=M%C3%A9todo%20replaceAll(),que%20passarmos%20por%20outro%20caractere.?utm_source=awin&utm_medium=site&utm_term=101248&awc=23465_1678467659_5f104ce2c64fa8a3922f21a302913942
+//https://github.com/Lea1dev/displayName-JavaScript/blob/main/script.js
+
+function tratamentoNomeCompleto(nomeCompleto) {
+    return nomeCompleto.replace(/ \s+/g, " ").trim();
+};
+
 function buttonSeparated(){
-    nomeCompleto = document.getElementById("campo-de-nome-completo").value.split(" ");
-    document.getElementById("campo-de-nome").value = nomeCompleto[0];
-    document.getElementById("campo-de-sobrenome").value = nomeCompleto[1];
+    let nomeCompleto = document.getElementById("campo-de-nome-completo").value;
+    let nomeCompletoTratado = this.tratamentoNomeCompleto(nomeCompleto);
+    let nomeCompletoSeparado = nomeCompletoTratado.split(" ");
+    document.getElementById("campo-de-nome").value = nomeCompletoSeparado[0];
+    let sobrenome = nomeCompletoSeparado.slice(1);
+
+    document.getElementById("campo-de-sobrenome").value = sobrenome.join(" ");
 };
