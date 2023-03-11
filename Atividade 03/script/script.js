f9005cdf1f21656c0723ec39f10102f4e3e4f109
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-03-11 00:08:58 -0300

    Update

--- Atividade 03/script/script.js
+++ Atividade 03/script/script.js
@@ -1,13 +1,18 @@
-var typeRobotrons = ["amarelo", "azul", "branco", "preto", "rosa", "vermelho"];
+var typeRobotrons = ["amarelo", "branco", "preto", "rosa", "vermelho", "azul"];
 
 var position = 0;
 
+if (position > typeRobotrons.length || position < 0){
+    position = 0;
+};
+
 function botaoDo(){
-    document.getElementsByClassName("robo")[0].src = "img/robotron-" + typeRobotrons[position];
-    //document.getElementsByClassName("robo").item
+    document.getElementsByClassName("robo")[0].src = "img/robotron-" + typeRobotrons[position] + ".png";
+    //document.getElementById("img-robo").src = "img/robotron-" + typeRobotrons[position] + ".png";
     position++;
 };
 
 function botaoUndo(){
-
+    document.getElementsByClassName("robo")[0].src = "img/robotron-" + typeRobotrons[position] + ".png";
+    position--;
 };
