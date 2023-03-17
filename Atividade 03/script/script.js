0cf44b5b68f7751d33301404e2775a23f3e24d8a
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-03-17 09:41:55 -0300

    atividade 04 incompleta

--- Atividade 03/script/script.js
+++ Atividade 03/script/script.js
@@ -1,3 +1,4 @@
+//Atividade 03
 var typeRobotrons = ["robotron", "robotron-amarelo", "robotron-branco", "robotron-preto", "robotron-rosa", "robotron-vermelho"];
 
 var positionActual = 0;
@@ -18,4 +19,56 @@ function botaoUndo(){
     validadePosition();
     positionActual--;
     document.getElementsByClassName("robo")[0].src = "img/" + typeRobotrons[positionActual] + ".png";
+};
+
+
+
+
+
+//Atividade 04
+
+// ================================SIMPLES=======================================
+//botão +
+const buttonPlus = document.getElementsByClassName("controle-ajuste");
+
+//botão -
+const buttonMinor = document.getElementsByClassName("controle-ajuste minor");
+
+buttonMinor[0].addEventListener("click", alert("test"));
+buttonPlus[0].addEventListener("click", alert("test"));
+
+
+// ================================COMPLETO=======================================
+
+//valor dos contadores (input.value)
+let counterValue = document.getElementsByClassName("controle-contador");
+
+//botão +
+let buttonPlus = document.getElementsByClassName("controle-ajuste");
+
+//botão -
+const buttonMinor = document.getElementsByClassName("controle-ajuste minor");
+
+/*
+for(let position = 0; position < buttonMinor.length; position++){
+    buttonMinor[position].addEventListener("click", actionOfButtonMinor(position));
+}
+
+for(let position = 0; position < buttonPlus.length; position++){
+    buttonPlus[position].addEventListener("click", actionOfButtonPlus(position));
+}
+*/
+
+for(let positionOfButton = 0; positionOfButton < buttonMinor.length; positionOfButton++){
+    buttonMinor[positionOfButton].addEventListener("click", (positionOfButton) => {
+        //Se o valor do contador da posição não for zero
+        if (!(counterValue[positionOfButton].value == 0))
+            counterValue[positionOfButton].value--;
+    });
+};
+
+for(let positionOfButton = 0; positionOfButton < buttonPlus.length; positionOfButton++){
+    buttonPlus[positionOfButton].addEventListener("click", (positionOfButton) => {
+        counterValue[positionOfButton].value++;
+    });
 };
