74f124a26d1a60a5234e8cdd34c6bcbbc84f7725
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-04-11 03:00:44 -0300

    :hammer: Adição da atividade 5

---
+++ Atividade 05/script.js
@@ -0,0 +1,8 @@
+
+const jsStyle = () => {
+    let mainText = document.getElementById('text');
+    mainText.style.fontFamily = 'Arial, sans-serif';
+    mainText.style.fontSize = '40px';
+    mainText.style.color = 'green';
+    mainText.style.textAlign = 'center'; //Adicional
+};
